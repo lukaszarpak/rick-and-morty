f08e14bfe7c8acd583c7d716f52f825aee9825dc
Łukasz Szarpak <lukasz.szarpak@plus.pl>
2021-04-23 15:45:34 +0200

    initial commit

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
+/* base */
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+/* UI components */
+import PrimeVue from 'primevue/config'
+
+createApp(App).use(PrimeVue).use(store).mount('#app')
